6ab8965cb682725dfbb2c202166e42788ecf5a24
Hafid <hafid.saou@gmail.com>
2023-11-26 17:37:05 +0100

    add an event in simpleStorage

--- backend/contracts/SimpleStorage.sol
+++ backend/contracts/SimpleStorage.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.18;
 
 contract SimpleStorage {
+     event NewValue(uint number);
     uint number = 5;
 
     function getNumber() external view returns (uint) {
@@ -10,6 +11,7 @@ contract SimpleStorage {
 
     function setNumber(uint _number) external {
         number = _number;
+        emit NewValue(number);
     }
 
     receive() external payable {} // to support receiving ETH by default
