1e5573fad0cea77ab819872e9b7337a8ea9e6b8b
Hafid <hafid.saou@gmail.com>
2023-12-01 22:17:10 +0100

    add sepolia in wagmi config and contrat address

--- frontend/wagmi.config.js
+++ frontend/wagmi.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "@wagmi/cli";
 import { hardhat } from "@wagmi/cli/plugins";
 import { react } from "@wagmi/cli/plugins";
-import { hardhat as hardhatChain } from "wagmi/chains";
+import { hardhat as hardhatChain, sepolia } from "wagmi/chains";
 
 export default defineConfig({
    out: "src/generated.ts",
@@ -12,6 +12,7 @@ export default defineConfig({
          deployments: {
             SimpleStorage: {
                [hardhatChain.id]: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+               [sepolia.id]: "0x20dfeE5209da3f38e8F7E3E6D9B35Fee24b265CB",
             },
          },
       }),
