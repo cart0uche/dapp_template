2a0e4b230472fde743f58972800945c575b943f3
Hafid <hafid.saou@gmail.com>
2023-12-01 22:18:04 +0100

    add sepolia in frontend

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -3,38 +3,41 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { hardhat } from "wagmi/chains";
+import { hardhat, sepolia } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const { chains, publicClient } = configureChains(
-  [hardhat],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+   [sepolia, hardhat],
+   [
+      alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
+      publicProvider(),
+   ]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  projectId: process.env.NEXT_PUBLIC_RAINBOW_APP_ID,
-  chains,
+   appName: "My RainbowKit App",
+   projectId: process.env.NEXT_PUBLIC_RAINBOW_APP_ID,
+   chains,
 });
 
 const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
+   autoConnect: true,
+   connectors,
+   publicClient,
 });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider chains={chains}>
-            <ChakraProvider>{children}</ChakraProvider>
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <body>
+            <WagmiConfig config={wagmiConfig}>
+               <RainbowKitProvider chains={chains}>
+                  <ChakraProvider>{children}</ChakraProvider>
+               </RainbowKitProvider>
+            </WagmiConfig>
+         </body>
+      </html>
+   );
 }
