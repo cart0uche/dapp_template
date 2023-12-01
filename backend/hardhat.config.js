c558b9bc595e157a3f228fe5ebb7744ffbc71230
Hafid <hafid.saou@gmail.com>
2023-12-01 22:11:18 +0100

    add sepolia network in hardhat config

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+   solidity: {
+      version: "0.8.18",
+      defaultNetwork: "localhost",
+      settings: {
+         optimizer: {
+            enabled: true,
+            // https://docs.soliditylang.org/en/latest/using-the-compiler.html#optimizer-options
+            runs: 200,
+         },
+      },
+   },
+
+   networks: {
+      sepolia: {
+         url: "https://sepolia.infura.io/v3/" + process.env.INFURA_KEY,
+         accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      },
+   },
 };
