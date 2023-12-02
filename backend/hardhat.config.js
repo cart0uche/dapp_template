0bbe36f2d0a6e17b8a54c0d4a663ed8325b652bd
Hafid <hafid.saou@gmail.com>
2023-12-02 16:58:00 +0100

    fix CI : set a default private key (from hardhat default account 0)

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const DEPLOYER_PRIVATE_KEY =
    process.env.DEPLOYER_PRIVATE_KEY ||
-   "0xfake1private2key3fake4private5key6fake7private8key";
+   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
